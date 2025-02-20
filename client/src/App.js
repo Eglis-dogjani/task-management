9949c7be1f597bca41594637726e691f855b58af
Eglis-dogjani <eglidogjani@gmail.com>
2025-02-20 11:54:15 -0800

    check dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Filterbar from './Components/Filterbar';
+import Navbar from './Components/Navbar';
+import { TaskProvider } from './Context/TaskContext';
+import Tasks from './Components/TaskList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <TaskProvider>
+                <Navbar />
+                <Filterbar />
+                <Tasks />
+            </TaskProvider>
+        </>
+    );
 }
 
 export default App;
